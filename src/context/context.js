5ae1f9e1b4f68476c66ce2d9939d05e26e8adbb8
hengzhengping <hengzhengping@gmail.com>
2022-05-10 21:29:08 +0800

    added search user component

--- src/context/context.js
+++ src/context/context.js
@@ -14,8 +14,49 @@ const GithubProvider = ({ children }) => {
     const [githubUser, setGithubUser] = useState(mockUser);
     const [repos, setRepos] = useState(mockRepos);
     const [followers, setFollowers] = useState(mockFollowers);
+    //request loading
+    const [requests, setRequests] = useState(0);
+    const [loading, setIsLoading] = useState(false);
+    //error
+    const [error, setError] = useState({ show: false, msg: "" })
 
-    return <GithubContext.Provider value={{ githubUser, repos, followers }}>{children}</GithubContext.Provider>
+    const searchGithubUser = async (user) => {
+        //toggleError
+        toggleError();
+        //setLoading(true)
+        const response = await axios(`${rootUrl}/users/${user}`)
+            .catch(err => console.log(err))
+        if (response) {
+            setGithubUser(response.data);
+        }
+        else {
+            toggleError(true, 'sorry, the user that you have search does not exist!');
+        }
+    };
+
+    //check rate
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`)
+            .then(({ data }) => {
+                let { rate: { remaining },
+                } = data;
+                setRequests(remaining);
+                if (remaining === 0) {
+                    toggleError(true, 'sorry, you have exceeded your hourly rate limit!');
+                }
+            })
+            .catch((err) => console.log(err));
+    };
+
+    function toggleError(show = false, msg = '') {
+        setError({ show, msg })
+    }
+    //error
+
+
+    useEffect(checkRequests, []);
+
+    return <GithubContext.Provider value={{ githubUser, repos, followers, requests, error, searchGithubUser }}>{children}</GithubContext.Provider>
 }
 
 export { GithubProvider, GithubContext };
